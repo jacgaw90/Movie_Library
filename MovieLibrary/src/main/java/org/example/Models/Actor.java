d0422f8285e5ce6bbf73129c5c153d2729a8861e
Jacek.Gawlik <jacek.gawlik@asseco.pl>
2024-03-01 14:41:54 +0100

    Add Actor class

---
+++ MovieLibrary/src/main/java/org/example/Models/Actor.java
@@ -0,0 +1,13 @@
+package org.example.Models;
+
+public class Actor {
+    private String firstName;
+
+    public java.lang.String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(java.lang.String firstName) {
+        this.firstName = firstName;
+    }
+}
