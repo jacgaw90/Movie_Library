17f8293073f46687883f98459a6db40e3fc928a9
Jacek.Gawlik <jacek.gawlik@asseco.pl>
2024-03-01 15:03:16 +0100

    Uzupełnienie Location.java

--- MovieLibrary/src/main/java/org/example/Models/Location.java
+++ MovieLibrary/src/main/java/org/example/Models/Location.java
@@ -1,1 +1,14 @@
+package org.example.Models;
+
+public class Location {
+    private String location;
+
+    public java.lang.String getLocation() {
+        return location;
+    }
+
+    public void setFirstName(java.lang.String location) {
+        this.location = location;
+    }
+}
 
